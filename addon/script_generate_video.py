023cd1ea9da97a9c6c795361b83f2b518b86f255
Danila <savin.dd@edu.spbstu.ru>
2020-07-06 18:46:39 +0300

    Тестим

--- addon/script_generate_video.py
+++ addon/script_generate_video.py
@@ -13,29 +13,39 @@ def add_object(name, verts, faces, edges=[], col_name='Video_col.1'):
     mesh.from_pydata(verts, edges, faces)
     return obj
 
-def add_basic_object(obj, counter, verts, faces, edges=[], col_name='Video_col.1'):
+def add_basic_object(obj, counter, edges=[], col_name='Video_col.1'):
     '''
     Добавление базового объекта
     '''
-    if obj['type'].lower() == 'cube':
-        # Добавляем куб
+    verts_c = [(0.5, 0.5, -0.5),
+             (0.5, -0.5, -0.5),
+             (-0.5, -0.5, -0.5),
+             (-0.5, 0.5, -0.5),
+             (0.5, 0.5, 0.5),
+             (0.5, -0.5, 0.5),
+             (-0.5, -0.5, 0.5),
+             (-0.5, 0.5, 0.5)]
+    faces_c = [(0, 1, 2, 3),
+             (4, 7, 6, 5),
+             (0, 4, 5, 1),
+             (1, 5, 6, 2),
+             (2, 6, 7, 3),
+             (4, 0, 3, 7)] 
+
+    verts_p = [(-0.5, -0.5, 0),
+                (0.5, -0.5, 0),
+                (-0.5, 0.5, 0),
+                (0.5, 0.5, 0)]
+    faces_p = [(0, 1, 3, 2)]
+
+    if (obj['type'].lower() == 'cube') or (obj['type'].lower() == 'paral'):
+        # Добавляем куб или параллелепипед
         #bpy.ops.mesh.primitive_cube_add(size=obj['size'], enter_editmode=False, location=(0, 0, 0))
-        ob = add_object(obj['id']+'.'+counter, verts, faces, edges, col_name)
-        
-    elif obj['type'].lower() == 'parallelepiped': # Пока в разработке
-        # Добавляем параллелепипед
-        bpy.ops.mesh.primitive_cube_add(size=1, enter_editmode=False, location=(0, 0, 0))
-        bpy.context.active_object.name = obj['id']+'.'+counter
-        ob = bpy.data.objects[obj['id']+'.'+counter]
-        ob.scale= (obj['scale'][0], obj['scale'][1], obj['scale'][2])
-        ob.data.name = obj['id']+'.'+counter
-        for obj in ob.users_collection[:]:
-            obj.objects.unlink(ob)
-        col = bpy.data.collections.get(col_name)
-        col.objects.link(ob)
+        ob = add_object(obj['id']+'.'+counter, verts_c, faces_c, edges, col_name)
+    
     elif obj['type'].lower() == 'sphere':
         # Добавляем сферу
-        bpy.ops.mesh.primitive_uv_sphere_add(radius=obj['size'], enter_editmode=False, location=(0, 0, 0))
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=obj['scale'][0], enter_editmode=False, location=(0, 0, 0))
         bpy.context.active_object.name = obj['id']+'.'+counter
         bpy.ops.object.shade_smooth()
         ob = bpy.data.objects[obj['id']+'.'+counter]
@@ -46,14 +56,7 @@ def add_basic_object(obj, counter, verts, faces, edges=[], col_name='Video_col.1
         col.objects.link(ob)
     elif obj['type'].lower() == 'plane':
         # Добавляем плоскость
-        bpy.ops.mesh.primitive_plane_add(size=obj['size'], enter_editmode=False, location=(0, 0, 0))
-        bpy.context.active_object.name = obj['id']+'.'+counter
-        ob = bpy.data.objects[obj['id']+'.'+counter]
-        ob.data.name = obj['id']+'.'+counter
-        for obj in ob.users_collection[:]:
-            obj.objects.unlink(ob)
-        col = bpy.data.collections.get(col_name)
-        col.objects.link(ob)
+        ob = add_object(obj['id']+'.'+counter, verts_p, faces_p, edges, col_name)
     return ob
 
 def animated_material(obj, color, cur_frame):
@@ -96,21 +99,6 @@ def generate_video(input_filepath, use_bezier):
     with open(input_filepath, 'r') as f:
         move = json.load(f)
 
-    verts = [(1.0, 1.0, 0.0),
-             (1.0, 0.0, 0.0),
-             (0.0, 0.0, 0.0),
-             (0.0, 1.0, 0.0),
-             (1.0, 1.0, 1.0),
-             (1.0, 0.0, 1.0),
-             (0.0, 0.0, 1.0),
-             (0.0, 1.0, 1.0)]
-    faces = [(0, 1, 2, 3),
-             (4, 7, 6, 5),
-             (0, 4, 5, 1),
-             (1, 5, 6, 2),
-             (2, 6, 7, 3),
-             (4, 0, 3, 7)] 
-
     # Создание сцены и объекта
     counter = 1
     for i in list(bpy.data.scenes.keys()):
@@ -124,14 +112,14 @@ def generate_video(input_filepath, use_bezier):
     # ------
 
     be = [] # Список существующих объектов
-    basic_objects = ['cube', 'plane', 'sphere', 'parallelepiped']
+    basic_objects = ['cube', 'plane', 'sphere', 'paral']
 
     # Расставляем объекты на сцене и прячем
     for time_frame in move:
         for obj in time_frame['obj']:
             if obj['id'] not in be:
                 if obj['type'].lower() in basic_objects:
-                    ob = add_basic_object(obj, counter, verts, faces, edges=[], col_name=newCol.name)
+                    ob = add_basic_object(obj, counter, edges=[], col_name=newCol.name)
                 else:
                     ob = add_object(obj['id']+'.'+counter, obj['size']['verts'], obj['size']['faces'],
                                     edges=[], col_name=newCol.name)
@@ -153,7 +141,17 @@ def generate_video(input_filepath, use_bezier):
             ob.rotation_euler.x = obj['rot'][0]
             ob.rotation_euler.y = obj['rot'][1]
             ob.rotation_euler.z = obj['rot'][2]
-            ob.scale = (obj['scale'][0], obj['scale'][1], obj['scale'][2])
+            scale_x = obj['scale'][0]
+            if len(obj['scale']) == 1:
+                scale_y = scale_x
+                scale_z = scale_x
+            elif len(obj['scale']) == 2:
+                scale_y = obj['scale'][1]
+                scale_z = 0
+            else:
+                scale_y = obj['scale'][1]
+                scale_z = obj['scale'][2]
+            ob.scale = (scale_x, scale_y, scale_z)
             
 
             ob.keyframe_insert(data_path="location", frame=fr, index=-1)
